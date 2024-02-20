058195b336a70ac2b76fd990dc10c937f3da512c
garcia roae <cgarciaroa@gmail.com>
2024-02-20 15:28:32 -0500

    Ultimo Commit

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/garciaroa/gambitcloud
+
+go 1.21.6
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.25.0
+	github.com/go-sql-driver/mysql v1.7.1
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.26.6 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.27.1
+	github.com/aws/smithy-go v1.20.0 // indirect
+	github.com/garciaroa/gambituser v0.0.0-20240219175523-d5d816a2f25e
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/garciaroa/gambitcloud/awsgo"
+	"github.com/garciaroa/gambitcloud/bd"
+	"github.com/garciaroa/gambitcloud/handlers"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) (*events.APIGatewayProxyResponse, error) {
+
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		panic("Error en los parametros. Debe enviar 'secretName', 'UrlPrefix' ")
+	}
+
+	var res *events.APIGatewayProxyResponse
+	prefix := os.Getenv("UrlPrefix")
+	path := strings.Replace(request.RawPath, prefix, "", -1)
+	method := request.RequestContext.HTTP.Method
+	body := request.Body
+	header := request.Headers
+
+	bd.ReadSecret()
+	status, message := handlers.Manejadores(path, method, body, header, request)
+
+	headersResp := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	res = &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       string(message),
+		Headers:    headersResp,
+	}
+
+	return res, nil
+}
+
+func ValidoParametros() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+	/*
+
+	   //ya no se van a utilizar
+	   	_, traeParametro = os.LookupEnv("UserPoolId")
+	   	if !traeParametro {
+	   		return traeParametro
+	   	}
+
+	   	_, traeParametro = os.LookupEnv("Region")
+	   	if !traeParametro {
+	   		return traeParametro
+	   	}*/
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+	return traeParametro
+}
