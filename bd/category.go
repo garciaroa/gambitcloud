de66b9947efe4f8293c7de403059abf3dfbaaecf
garcia roae <cgarciaroa@gmail.com>
2024-02-29 11:48:18 -0500

    Ultimo Commit

--- bd/category.go
+++ bd/category.go
@@ -3,8 +3,11 @@ package bd
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/garciaroa/gambitcloud/models"
+	"github.com/garciaroa/gambitcloud/tools"
 )
 
 func InsertCategory(c models.Category) (int64, error) {
@@ -30,4 +33,35 @@ func InsertCategory(c models.Category) (int64, error) {
 	}
 
 	return LastInsertId, nil
+}
+
+func UpdateCategory(c models.Category) error {
+	fmt.Println("Comienza registro de updateCategory")
+
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "Update category SET "
+	if len(c.CategName) > 0 {
+		sentencia += " Categ_name = '" + tools.EscapeString(c.CategName) + "'"
+	}
+	if len(c.CategPath) > 0 {
+		if !strings.HasSuffix(sentencia, "SET ") {
+			sentencia += ", "
+		}
+		sentencia += "Categ_Path= '" + tools.EscapeString(c.CategPath) + "'"
+	}
+	sentencia += " WHERE Categ_Id = " + strconv.Itoa(c.CategID)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("Update Category > Ejecucion Exitosa")
+	return nil
 }
